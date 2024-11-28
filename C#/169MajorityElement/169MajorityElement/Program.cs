036a74021b6635bfc3e4cbdb4edcc9f02ea56dfc
Marek Štec <mar.stec@seznam.cz>
2024-11-28 14:45:50 +0100

    Update

---
+++ C#/169MajorityElement/169MajorityElement/Program.cs
@@ -0,0 +1,28 @@
+﻿int MajorityElement(int[] nums)
+{
+    int candidate = nums[0];
+    int count = 1;
+    foreach (int item in nums[1..])
+    {
+        if (count <= 0)
+        {
+            candidate = item;
+            count = 1;
+        }
+        else if (candidate == item)
+            count++;
+        else
+            count--;
+    }
+    return candidate;
+}
+
+
+
+
+int[] nums = [1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 5, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 6, 6, 61, 1, 1, 81, 1, 1, 1, 16];
+Console.WriteLine(MajorityElement(nums));
+
+nums = [3, 2, 3];
+Console.WriteLine(MajorityElement(nums));
+
---
+++ C#/229MajorityElelment2/229MajorityElelment2/Program.cs
@@ -0,0 +1,45 @@
+﻿IList<int> MajorityElement(int[] nums)
+{
+    int k = 3;
+    Dictionary<int, int> candidates = [];
+    foreach (int item in nums)
+    {
+        if (candidates.ContainsKey(item))
+            candidates[item]++;
+        else if (candidates.Keys.Count < k - 1)
+            candidates[item] = 1;
+        else
+        {
+            foreach (var kvp in candidates)
+            {
+                if (kvp.Value == 1)
+                    candidates.Remove(kvp.Key);
+                else
+                    candidates[kvp.Key]--;
+            }
+        }
+    }
+    List<int> result = new List<int>();
+    foreach (var kvp in candidates)
+    {
+        if (nums.Count(n => n == kvp.Key) > Math.Floor(nums.Length / (double)k))
+            result.Add(kvp.Key);
+    }
+    return result;
+}
+
+
+int[] nums = [1, 1, 1, 1, 1, 12, 2, 2, 2, 2, 2, 2, 26, 9, 1];
+foreach (int item in MajorityElement(nums))
+{
+    Console.Write(item);
+    Console.Write(", ");
+}
+Console.WriteLine();
+
+nums = [3, 2, 3];
+foreach (int item in MajorityElement(nums))
+{
+    Console.Write(item);
+    Console.Write(", ");
+}
